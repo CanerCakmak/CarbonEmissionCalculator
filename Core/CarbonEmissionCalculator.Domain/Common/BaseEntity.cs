6a396d1f73f92a50771ea6a047eb1a0f9838bec5
Caner <canercakmak153@gmail.com>
2025-06-09 15:15:12 +0300

    Base Entity ve Sql Connection String yazıldı.

--- Core/CarbonEmissionCalculator.Domain/Common/BaseEntity.cs
+++ Core/CarbonEmissionCalculator.Domain/Common/BaseEntity.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CarbonEmissionCalculator.Domain.Common
+﻿namespace CarbonEmissionCalculator.Domain.Common
 {
     public class BaseEntity
     {
